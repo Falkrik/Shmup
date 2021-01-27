922789f1afed4746e94d2370684241a125c1f2f5
Falkor <tobaksduo@gmail.com>
2021-01-28 00:00:14 +0100

    Minor changes to movement variables

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    [Header("Player")]
+    private const float defaultSpeed = 7.0f;
+    private float speed;
+    [Space]
+    private int maxHealth = 6;  // temporary
+    private int curHealth;
 
-    private const float defaultVelocity = 7.0f;
 
     [Header("Tools")]
     private Rigidbody2D rb2d;
 
     private void Start()
     {
+        speed = defaultSpeed;
+        curHealth = maxHealth;
         rb2d = GetComponent<Rigidbody2D>();
     }
 
@@ -26,6 +33,12 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        rb2d.velocity = input * defaultVelocity;
+        rb2d.velocity = input * speed;
+    }
+
+
+    public void IncreaseSpeed(float amount)
+    {
+        speed += amount;
     }
 }
